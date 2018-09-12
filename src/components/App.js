9541ee5578fe30a8c3bd8cd4191ecacd658ee6c4
Elly Maimon and Ryan Murry <elly@>
2018-09-12 11:36:02 -0700

    handle state change to set coach names and show game instead of menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import Menu from './Menu';
+import Game from './Game';
 import '../styles/App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      twoPlayerGame: false
+      twoPlayerGame: false,
+      menuVisible: true,
+      coach1: '',
+      coach2: ''
     };
     this.handleTwoPlayerClick = this.handleTwoPlayerClick.bind(this);
     this.handleOnePlayerClick = this.handleOnePlayerClick.bind(this);
+    this.handleStartGameClick = this.handleStartGameClick.bind(this);
   }
 
   handleTwoPlayerClick() {
@@ -24,11 +29,28 @@ class App extends Component {
     })
   }
 
+  handleStartGameClick(coach1Name, coach2Name) {
+    this.setState({
+      menuVisible: false,
+      coach1: coach1Name,
+      coach2: coach2Name
+    })
+  }
+
 
   render() {
+    let currentView = null;
+    if (this.state.menuVisible) {
+      currentView = <Menu twoPlayerGame={this.state.twoPlayerGame}
+                          onTwoPlayerClick={this.handleTwoPlayerClick}
+                          onOnePlayerClick={this.handleOnePlayerClick}
+                          onStartGameClick={this.handleStartGameClick}/>;
+    } else {
+      currentView = <Game />
+    }
     return (
       <div className='app'>
-        <Menu twoPlayerGame={this.state.twoPlayerGame} onTwoPlayerClick={this.handleTwoPlayerClick} onOnePlayerClick={this.handleOnePlayerClick}/>
+        {currentView}
       </div>
     );
   }
