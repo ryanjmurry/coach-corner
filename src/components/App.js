233b5915469e4e7607ab0b17801fabc6dfd0e577
Elly Maimon and Ryan Murry <ryan@>
2018-09-12 11:14:36 -0700

    allow user to toggle between one and two player game and assign names to coaches

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,30 @@ import '../styles/App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      twoPlayerGame: false
+    };
+    this.handleTwoPlayerClick = this.handleTwoPlayerClick.bind(this);
+    this.handleOnePlayerClick = this.handleOnePlayerClick.bind(this);
+  }
+
+  handleTwoPlayerClick() {
+    this.setState({
+      twoPlayerGame: true
+    })
+  }
+
+  handleOnePlayerClick() {
+    this.setState({
+      twoPlayerGame: false
+    })
   }
 
 
   render() {
     return (
       <div className='app'>
-        <Menu />
+        <Menu twoPlayerGame={this.state.twoPlayerGame} onTwoPlayerClick={this.handleTwoPlayerClick} onOnePlayerClick={this.handleOnePlayerClick}/>
       </div>
     );
   }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import themeSong from '../assets/theme.mp3';
+import { Icon, Form, Button } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 
 // Styles
 const title = {
@@ -9,23 +11,63 @@ const title = {
 }
 // End Styles
 
-const song = new Audio(themeSong);
-
-const toggleMusic = () => {
-  if(song.paused) {
-    song.play();
-  } else {
-    song.pause();
-  }
-}
-
 const Menu = (props) => {
+  const song = new Audio(themeSong);
+
+  const toggleMusic = () => {
+    if(song.paused) {
+      song.play();
+    } else {
+      song.pause();
+    }
+  }
+
+  const handleOnePlayerClick = () => {
+    props.onOnePlayerClick();
+  }
+
+  const handleTwoPlayerClick = () => {
+    props.onTwoPlayerClick();
+  }
+
+  const handleNewGameSubmission = (e) => {
+      let coach2Name = e.target.coach2 === undefined ? '' : e.target.coach2.value;
+      let newGame = {
+      // if(e.target.coach2.value !== undefined) return coach2 = e.target.coach2.value;
+      coach1: e.target.coach1.value,
+      coach2: coach2Name
+    }
+    console.log(newGame)
+  }
+
+  let currentView = null;
+  if (props.twoPlayerGame) {
+    currentView = <Form.Input label='Coach 2 Name' name='coach2' placeholder='Coach 2 Name' required/>
+  } else {
+    currentView = null;
+  }
+
   return (
     <div>
       <h1 style={title}>Coach's Corner!</h1>
-      <button onClick={toggleMusic}>Start Game</button>
+      <Icon name='volume up' onClick={toggleMusic} />
+      <Button onClick={handleOnePlayerClick}>One Player Game</Button>
+      <Button onClick={handleTwoPlayerClick}>Two Player Game</Button>
+        <Form onSubmit={handleNewGameSubmission}>
+          <Form.Group widths='equal'>
+            <Form.Input label='Coach 1 Name' name='coach1' placeholder='Coach 1 Name' required/>
+            {currentView}
+          </Form.Group>
+          <Form.Button>Start Game</Form.Button>
+        </Form>
     </div>
   );
+}
+
+Menu.propTypes = {
+  twoPlayerGame: PropTypes.bool.isRequired,
+  onTwoPlayerClick: PropTypes.func.isRequired,
+  onOnePlayerClick: PropTypes.func.isRequired
 }
 
 export default Menu
