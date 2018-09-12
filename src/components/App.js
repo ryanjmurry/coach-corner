215810013f33b26fa3e2b741e871c1e50b4ab4bf
Elly Maimon and Ryan Murry <ryan@>
2018-09-12 10:35:06 -0700

    move sound functionality into menu component and allow toggle on button click

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,21 @@
 import React, { Component } from 'react';
 import Menu from './Menu';
-import themeSong from '../assets/theme.mp3'
+import '../styles/App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.pauseMusic = this.pauseMusic.bind(this);
-    this.song = new Audio(themeSong);
   }
 
-
-  pauseMusic() {
-     this.song.pause();
-  }
 
   render() {
-    // this.song.play();
     return (
-      <div>
+      <div className='app'>
         <Menu />
-        <button onClick={this.pauseMusic}>Start Game</button>
       </div>
     );
   }
 }
-
-
 
 export default App;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import themeSong from '../assets/theme.mp3';
+
+// Styles
+const title = {
+  fontFamily: 'Graduate',
+  textAlign: 'center',
+  fontSize: '4em'
+}
+// End Styles
+
+const song = new Audio(themeSong);
+
+const toggleMusic = () => {
+  if(song.paused) {
+    song.play();
+  } else {
+    song.pause();
+  }
+}
 
 const Menu = (props) => {
   return (
-    <p>Menu works!</p>
-  )
+    <div>
+      <h1 style={title}>Coach's Corner!</h1>
+      <button onClick={toggleMusic}>Start Game</button>
+    </div>
+  );
 }
 
 export default Menu
